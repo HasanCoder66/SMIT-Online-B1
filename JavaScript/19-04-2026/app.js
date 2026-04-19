9074c401d08be988faab4c2712c5df0e6af9e845
hasanCoder66 <hasanashraf874@gmail.com>
2026-04-19 11:14:48 +0500

    js class 1

---
+++ JavaScript/19-04-2026/app.js
@@ -0,0 +1,170 @@
+// javascript is a high level interpreted programming lanaguage.
+
+// console.log("Hello world");   // read => agy samjhaye ga
+// console.log("Hello world");   // read => agy samjhaye ga computer ko samjahyega
+// console.log("Hello world");
+
+// programming languages ==>
+// javascript , python, c++, c sharp 
+
+
+// syntax ==> code likhny ka tareeeka
+
+
+// let username = "hasan"
+
+// console.log(username);
+
+
+// Variables (let, const, var)
+
+// variables kia hoty hain ? 
+// Data store kerny ke liye container hoty hai.
+
+
+// variable create kerny ke tareeky. 3
+// variable keywords 
+// var 
+// let
+// const
+
+
+
+//variable syntax 
+
+// keyword , variable name , = value 
+
+// let bottle = "Pakola"
+// console.log(bottle);
+
+
+// Data Types ==>
+
+// 1. Primitive Types  ==> 
+
+// String (text),           plain text wo string kehlata hai.
+// Number 3,                0, 9 integar.
+// Boolean (true/false),    true / false 
+// Null,                    null
+// Undefined                undefined    // developer galti sai bi variable ki value nhi
+//  deta undefined system generated value hoti hai undefined
+
+
+
+
+
+// let username = "hasan"
+
+// console.log(undefined)
+
+// let user;
+// console.log(user);
+
+// variable for string
+let fullName = "Muhammad Hasan";  
+// console.log(fullName);
+
+// variable for num
+let n1 = 12; 
+// console.log(n1);
+
+// variable for boolean
+let isUser = false;  
+// console.log(typeof isUser);
+
+
+// variable for null
+let user = null;    
+// console.log(user);
+
+
+// variable for undefined
+let bottle;    
+// console.log(bottle);
+
+
+
+
+
+
+
+
+// 2. Non-Primitive ya Reference Type   ==>  brackets  (), [], {}
+// Object, {}
+// Array, []
+// Function, ()
+
+
+
+// 5. Operators
+let a = 34;
+// console.log(a + 1 + 1);
+
+// const b= 22;
+
+// console.log(a + b);  Addition
+// console.log(a - b);    //subtraction
+// console.log(a * b);    //mutliplications
+// console.log(a / b);    // Divide
+
+
+
+// let isBottle = true;
+// console.log(isBottle);
+
+// let user = null;
+// console.log(myNull);
+
+
+
+// Increment   // add kerna
+// Decrement   // kam kerna
+
+// Pre Increment  // pehly add kerna
+// Post Increment // baad mai add kerna
+
+// Pre Decrement  // pehly kam kerna
+// Post Decrement // baad mai kam kerna
+
+
+// ++ || -- 
+
+// ++a
+
+// console.log(a++);
+
+
+// console.log(++a);
+// console.log(a);
+
+
+
+
+// % modulus
+
+// bacha wa deta hai isy aap remainder bi keh sekty hain 
+
+
+
+// Comparison Operators  
+
+// values ko aapas mai compare kerny ke liye hum comparision operator ka use kerty hain.
+
+
+//  > greater then 
+/*  < less then  */
+//  >= is greater than or equal  
+//  <= is less than or equal
+
+// ==  data type ko check nhi kerta ==> only values ko check kerta hai...
+// === strict equal to ==> data type ko bi check kerta hai... or value ko biii.
+
+
+
+let num1 = 23;
+let num2 = "23";
+
+console.log("Number 1 ==>",typeof num1);
+console.log("Number 2 ==>",typeof num2);
+
+console.log("Num or Num 2 are",num1 === num2);
