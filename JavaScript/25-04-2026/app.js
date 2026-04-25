c5a8dbd8486ecf851ed90412d44a40797561867b
hasanCoder66 <hasanashraf874@gmail.com>
2026-04-25 10:53:11 +0500

    Responsive fb login, signup pages, revision last class

---
+++ JavaScript/25-04-2026/app.js
@@ -0,0 +1,41 @@
+// console.log("hello world!");
+
+
+// variables  
+
+// store ya container 
+
+// values  ==> data types ==> primitives | non primitives
+
+
+// 1. Primitives Data Types ==> 
+
+// ""           // string
+// 2            // number
+// true | false // boolean
+// null        // null
+// undefined   // undefined
+
+
+// 2. Non Primitives | Reference Data Types ==>
+// (), [], {}
+// array, object, function
+
+
+// let username = "muhammad hasan"     // string value
+// let age = 23                        // number value
+// let isAdmin = false                 // boolean value
+// let user = null                     // null
+
+// let a;                              // undefined
+
+
+// console.log(typeof isAdmin);
+
+
+let n1 = 22;
+let n2 = 12;
+
+// modulus operator in js % ==> remainder deta ==> remainder kia hota hai ==> remainder us value ko keh rahy jo operation perform kernay ke baad mil rahi hai...
+
+console.log(n1 % n2);  // 0
