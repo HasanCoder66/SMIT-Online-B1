b8430e8e76db648afb69c2242f5697fb8b1525a2
hasanCoder66 <hasanashraf874@gmail.com>
2026-05-03 11:05:11 +0500

    Revision switch, loops for, while, do while, functions, default params, arg, param,

---
+++ JavaScript/class-4/app.js
@@ -0,0 +1,371 @@
+//  console.log("js is running -->");
+
+// switch
+
+
+// for loop
+// while loop
+// do while
+// break & continue
+
+// Project:
+
+// Number guessing game
+// Table generator
+
+
+// Functions
+// parameters
+// return
+// arrow functions
+// default parameters
+
+
+
+// abi tak nhi bataya ke aapny kab chlna haii .. 
+
+// let isAdmin = false;
+
+// console.log(isAdmin);
+
+
+// function 1 esa piece of code ya phir block of code hoga.. jabh mai call keronga tab chly ga..
+// function reuseble hoty hai...
+// arguements
+// parameter
+
+
+// syntax ==>
+
+// keyword function | function name | (parameter) | {} curly brackets ==> logic | implementation | execution | block of code | scope
+
+
+// function ko call kerny ke liye us ky par hona zaroori hain ya phir invoke  par == ()
+
+
+// function sum (){
+//     console.log(2 + 2);
+// }
+
+// sum()
+
+// paremters kiya hoty hain ?
+// paremters values ko recieve kerny ka kaam kerty hain .. 
+
+// ab ye value kahan sai atii hai 
+// ye value arguement bhejta hai or parameter ka kaam recieve kerna hota hai.. 
+
+
+
+// arguements kia hoty hain ?
+
+// jabh bi function chly ga to agar koi value leker jani hai to to arguements jayge... 
+
+                // paremeters
+// function subtract (n1, n2 = 5){
+//     console.log("after Subtraction", n1 - n2);
+// }
+
+// subtract(20, 15)   /// arguements 
+
+
+
+// function paniPinaHai (brand){
+//     console.log("Pani pina hai",brand);
+// }
+
+// // paniPinaHai("aquafina")
+// paniPinaHai("nestle")
+
+
+
+// function calculation (a,b){
+
+//     let result = a + b;
+//     return result
+// }
+
+// let fnResult = calculation(45,12)
+
+// console.log(fnResult + 132);
+
+// 2 tarha fn dekh liye hain humny 
+
+// pehla : jo kuch bi return nhi kerta haii..
+// dosra : jo kuch return kerta hai
+
+// return kahan milta hai..
+
+// return jab milta hai jab function call hota hai...
+
+
+// jabh function kuch bi return nhi kerta to undefined milta hai 
+
+// without return 
+// function foo (){
+
+// }
+
+// let khali = foo()
+
+// console.log(khali);
+
+// with return function   ==>
+
+
+// function nameBaharBhejhdo (){
+//     return "Shoaib Jani.."
+// }
+
+// let name = nameBaharBhejhdo()
+
+// console.log(name);
+
+
+// switch statements ==>
+
+
+// syntax 
+
+// switch condition execution | logic  
+// keyword | () | {}
+
+// let waterBottleLtr = 2;
+
+// switch (waterBottleLtr){
+//     case 1:
+//         console.log("Paani ki 1 ltr bottle 100 rupy ki hai...");
+
+//         default : 
+//         console.log("default chl raha hai...");
+// }
+
+
+
+// let tabs = ["general", "settings", "alerts"]
+// let activeTab = tabs[1]
+
+
+
+// switch(activeTab){
+//     case "general":
+//         console.log("general tab ui");
+//         break;
+
+//         case "settings":
+//             console.log("setting tab ui");
+//             break;
+
+//             case "alerts":
+//                 console.log("alerts tab ui");
+//                 break;
+
+//                 default:
+//                     console.log("basic ui hai....");
+// }
+
+
+
+// if  true base if run hota hai..
+
+// false agar hoga if ki condition mai to else chlta tha... 
+
+
+
+
+// loop hota kia hai ? 
+
+// kisi bi kaam ke repeat hony ho ko loop kehty hain 
+
+
+// reels  1 -- > 2 --> 3 -- > 
+
+// ui change 
+
+// card  --> post reel person, description section, comments, playback , like, share
+
+
+
+
+// mery pass 1 website hai .. 
+
+
+// for loop   ==> syntax for(){}
+
+// iterations  
+
+
+// keyword ==> for  | () condition | {} curly brackets ==>  execution | logic | implentionation | scope
+
+// condition  
+// start | end | change 
+
+// start wo jagah hogi condition mai jahan sai loop run kerna start kery ga .. 
+// end wo jagah hogi condition mai jahan sai loop stop hoga .. 
+// change loop mai esi cheez jis ki madad sai loop chlta hai...   ==> increment -- decrement
+
+
+
+// var initialization   == start
+// condition ==>  i < 2 == end
+// increment or decrement == change
+
+// for(let i = 11; i < 16; i++){
+// console.log("hello world", i);
+// }
+
+
+// let tableNum = 5;
+
+// console.log(tableNum , "X" , 1 , "=", tableNum * 1);
+// console.log(tableNum , "X" , 2 , "=", tableNum * 2);
+// console.log(tableNum , "X" , 3 , "=", tableNum * 3);
+// console.log(tableNum , "X" , 4 , "=", tableNum * 4);
+// console.log(tableNum , "X" , 5 , "=", tableNum * 5);
+// console.log(tableNum , "X" , 6 , "=", tableNum * 6);
+// console.log(tableNum , "X" , 7 , "=", tableNum * 7);
+// console.log(tableNum , "X" , 8 , "=", tableNum * 8);
+// console.log(tableNum , "X" , 9 , "=", tableNum * 9);
+// console.log(tableNum , "X" , 10 , "=", tableNum * 10);
+
+
+// table using  looop  ==>
+
+
+// for (let i = 1; i < 11; i++){
+//     console.log(tableNum, "X",i, "=", tableNum * i);
+// }
+
+
+// for loop execution ==>
+
+// variable initialize howa..
+// condition check hotii.. 
+// block of code run howa..
+// after execution increment or decrement hogaa.. 
+
+
+
+
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+
+// break a loop 
+
+
+// for (let i = 0 ; i < 4; i++){
+    
+//     if(i == 1){
+//         console.log(i);
+//         break
+//     }
+
+//     // if(i == 2){
+//     //     console.log(i);
+//     //     break
+//     //     console.log(i);
+//     // }
+   
+// }
+
+
+// Arrays ==>
+
+// mixed array ==> primitives or non primitves | reference
+// let students = ["shoaib", "fahad", "ali","saqib", "hasan","ahsan"]  // string array ==> 
+
+
+// for(let i = 0; i < students.length; i++){
+//     if(students[i] == "hasan"){
+//         console.log("hello", students[i]);
+//         break
+//     }else {
+//         console.log("han dost ==>");
+//     }
+// }
+
+
+
+// reverse loop ==>
+
+// for(let i = 10; i >= 1; i--){
+// console.log(i);
+// }
+
+
+
+
+// let nums = [12,34,56,78,910]
+// console.log(students[0]);
+// console.log(students[1]);
+// console.log(students[2]);
+// console.log(students[3]);
+
+
+
+
+// let stu1 = "uzair"
+// let stu2  = "shakoor"
+// let stu3  = "saleem"
+
+
+// console.log(stu2);
+
+
+
+// while loop
+
+
+// while loop organized version hai for loop ka ==>
+
+
+
+// variable initialize howa..
+// condition check hotii.. 
+// block of code run howa..
+// after execution increment or decrement hogaa.. 
+
+
+// let i = 0;
+// while(i<2){
+//     if(i == 1){
+//         break;
+//     }
+// console.log("han dost loop chl raha haii...", i);
+//     i++
+// }
+
+
+// do while ==> 
+
+// let i = 0;  // variable initialization 
+
+// do{
+//     // execution ==>
+//     console.log("pani pilaaa do");
+
+//     // incremnent or decrement 
+//     i++
+// } 
+// // condition
+// while(i == 0)
+
+
+
+// while loop kesy chl raha hai.. 
+// while loop ki execute organized version hai.. for loop ka ...
+
+
+
+// do while 
+
+// varibale initailization 
+// execution  do{} 
+// increment or decrement 
+// condition 
+
+
+// break & continue
